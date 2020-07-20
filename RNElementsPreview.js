a18b83e88a200b9a786b53e5d884a84f751d38db
Alin Salaru <mplacintasalaru@deloittece.com>
2020-07-20 11:52:16 +0300

    working with rnelems'

--- RNElementsPreview.js
+++ RNElementsPreview.js
@@ -1,11 +1,37 @@
 import {Button, ThemeProvider} from 'react-native-elements';
 import React from 'react';
-import {Avatar} from 'react-native-elements';
+import {AirbnbRating} from 'react-native-elements';
 
+const theme = {
+  Button: {
+    raised: true,
+    containerStyle: {
+      margin: 10,
+    },
+  },
+};
 
 const RNElementsPreview = () => {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <AirbnbRating
+        count={11}
+        reviews={[
+          'Terrible',
+          'Bad',
+          'Meh',
+          'OK',
+          'Good',
+          'Hmm...',
+          'Very Good',
+          'Wow',
+          'Amazing',
+          'Unbelievable',
+          'Jesus',
+        ]}
+        defaultRating={11}
+        size={20}
+      />
       <Button title="Hey!" />
     </ThemeProvider>
   );
